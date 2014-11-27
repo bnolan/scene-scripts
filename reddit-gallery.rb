c948fca46dfbb628d0a1bfe107432bb3628f896f
Ben Nolan <ben@nolanconsul.com>
2014-11-27 22:41:57 +1300

    Added color lovers. Corrected scale for rift mode.

--- reddit-gallery.rb
+++ reddit-gallery.rb
@@ -1,5 +1,5 @@
-# Generate a gallery from an image gallery subreddit. Requires imagemagick. Assumes the billboards are rendered
-# at 512x512.
+# Generate a gallery from an image gallery subreddit. Requires imagemagick to be installed in your
+# path. Assumes the billboards are rendered at 512x512.
 
 require 'open-uri'
 require 'json'
@@ -8,7 +8,7 @@ require 'uri'
 # For the vector class
 require 'gmath3D'
 
-SUBREDDIT = "retrogameporn".downcase
+SUBREDDIT = "macroporn".downcase
 DOWNLOAD = true
 
 puts "Fetching /r/#{SUBREDDIT} as json..."
@@ -48,16 +48,16 @@ json["data"]["children"].each do |child|
   x = i % 5
   z = (i / 5).floor
 
-  v = GMath3D::Vector3.new(x, 0, -z) * 10
-  v += GMath3D::Vector3.new(5, 2.5, -5)
+  v = GMath3D::Vector3.new(x, 0, -z) * 5
+  v += GMath3D::Vector3.new(5, 1.5, -5)
 
   height = `identify scenes/images/r-#{SUBREDDIT}-#{i}#{extension}`.match(/x(\d+)/)[1].to_i
-  margin = (512 - 20 - height) / 2
+  margin = (512 - 40 - height) / 2
 
   xml += <<-EOF
-    <billboard position="#{v.x} #{v.y} #{v.z}" rotation="0 0 0" scale="5 5 0.5">
+    <billboard position="#{v.x} #{v.y} #{v.z}" rotation="0 0 0" scale="3 3 0.3">
       <![CDATA[
-        <center style="margin-top: #{margin}px">
+        <center style="margin-top: #{margin}px; font-size: 2em">
           <img src="/images/r-#{SUBREDDIT}-#{i}#{extension}" style="max-width: 100%" /><br />
           #{title}
         </center>
